d78d19c9d4d79208deb7ea5e702bf6d2de3d2764
Jeff Papiez <jpapiez@live.com>
2025-09-01 18:25:39 -0700

    chore(client): robust Uri parsing for ApiBaseUrl; use using-var HttpClient in bootstrap; tests green

--- src/client/Program.cs
+++ src/client/Program.cs
@@ -6,7 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 // Load configuration
-var configHttp = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+using var configHttp = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 
 using var response = await configHttp.GetAsync(new Uri("appsettings.json", UriKind.Relative));
 using var stream = await response.Content.ReadAsStreamAsync();
@@ -22,11 +22,23 @@ if (builder.HostEnvironment.IsDevelopment())
     }
 }
 
-configHttp.Dispose();
-
 // Configure HttpClient with API base URL
-var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
+var apiBaseValue = builder.Configuration["ApiBaseUrl"];
+Uri apiBaseUri;
+if (!string.IsNullOrWhiteSpace(apiBaseValue) && Uri.TryCreate(apiBaseValue, UriKind.Absolute, out var abs))
+{
+    apiBaseUri = abs;
+}
+else if (!string.IsNullOrWhiteSpace(apiBaseValue) && Uri.TryCreate(apiBaseValue, UriKind.Relative, out var rel))
+{
+    apiBaseUri = new Uri(new Uri(builder.HostEnvironment.BaseAddress), rel);
+}
+else
+{
+    apiBaseUri = new Uri(builder.HostEnvironment.BaseAddress);
+}
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = apiBaseUri });
 
 builder.Services.AddScoped<ToastService>();
 builder.Services.AddScoped<RealtimeService>();
